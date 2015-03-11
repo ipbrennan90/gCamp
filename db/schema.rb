ba1473a30832e7434cbab4472bf7fb9d79b55e4b
Ian Brennan <ian.brennan@icloud.com>
2015-03-11 14:53:33 -0600

    added comments table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150307031438) do
+ActiveRecord::Schema.define(version: 20150311205300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string  "content"
+    t.integer "task_id"
+  end
 
   create_table "memberships", force: :cascade do |t|
     t.integer "user_id",    null: false
