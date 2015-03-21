af61712f6ae2ee9c2019ef3e4e3b9f0773b82b9e
Ian Brennan <ian.brennan@icloud.com>
2015-03-21 13:29:00 +0500

    getting tests to pass

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,7 @@ class UsersController < InternalController
   def set_user
     @user = User.find(params[:id])
   end
-
+  
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :permission)
   end
